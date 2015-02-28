b71f509b9182f2c2d1ee60022ec429337464b3d7
Al Johri <al.johri@gmail.com>
2015-02-28 15:48:07 -0600

    add jeopardy data

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-import os
+import os, csv
 from flask import Flask, request, redirect
 from flask import Response
 import twilio.twiml
 
 app = Flask(__name__)
 
+jmap = {}
+
+with open("jeopardy.csv") as f:
+	reader = csv.DictReader(f)
+	for row in reader:
+		jmap[row['clue']] = row['answer']
+
 @app.route("/", methods=['GET', 'POST'])
 def respond():
-    resp = twilio.twiml.Response()
-    resp.message("Your (%s) question was, %s." % (request.values.get('From'), request.values.get('Body')))
-    return Response(str(resp), mimetype='text/xml')
+	resp = twilio.twiml.Response()
+
+	if request.values.get('Body'):
+		question = request.values.get('Body').split("\n")[-1]
+	else:
+		return "question cannot be parsed: %s" % request.values.get('Body')
+
+	if jmap.get('question') == None:
+		return "question not found in jeopardy: %s" % question
+	else:
+		resp.message("What is %s?" % (jmap['question']))
+		return Response(str(resp), mimetype='text/xml')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
